2345574c254ce4025ad2edf4ea5fd05d068f4965
Jack Moran <jvckmorvn@gmail.com>
2022-07-15 13:46:08 +0100

    Implemented movie list and delete button

---
+++ src/components/movies.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import { getMovies } from "../services/fakeMovieService";
+
+class Movies extends Component {
+  state = {
+    movies: getMovies(),
+  };
+
+  handleDelete = (movie) => {
+    const movies = this.state.movies.filter((m) => m !== movie);
+    this.setState({ movies });
+  };
+
+  render() {
+    return (
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Genre</th>
+            <th>Stock</th>
+            <th>Rate (£/Day)</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.state.movies.map((movie) => (
+            <tr key={movie._id}>
+              <td>{movie.title}</td>
+              <td>{movie.genre.name}</td>
+              <td>{movie.numberInStock}</td>
+              <td>{movie.dailyRentalRate}</td>
+              <td>
+                <button
+                  onClick={() => this.handleDelete(movie)}
+                  className="btn btn-danger btn-sm"
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+export default Movies;
+
+// {this.movies.map((movie) => (
+//   <Movie key={movie.id} onDelete={this.handleDelete} movie={movie} />
+// ))}
