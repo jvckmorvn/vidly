3996268371f137f2f4a638baca23d13535dd0519
Jack Moran <jackmoran@192.168.1.179>
2022-07-25 15:43:09 +0100

    Implemented navbar and routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
 import Movies from "./components/movies";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
+import NavBar from "./components/navBar";
 import "./App.css";
 
 function App() {
   return (
-    <main className="container">
-      <Movies />
-    </main>
+    <React.Fragment>
+      <NavBar />
+      <main className="container">
+        <Switch>
+          <Route path="/movies" exact component={Movies} />
+          <Route path="/customers/:id?" component={Customers} />
+          <Route path="/rentals/:id?" component={Rentals} />
+          <Route path="/not-found" component={NotFound} />
+          <Redirect from="/" exact to="/movies" />
+          <Redirect to="/not-found" />
+        </Switch>
+      </main>
+    </React.Fragment>
   );
 }
 
